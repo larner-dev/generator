2428d6d118ab649b1370b6efdd9979c1d5bcea5b
Aaron Larner <alarner@users.noreply.github.com>
2023-01-11 14:13:24 -0800

    Manually copy .gitignore file for typescript-nodejs

--- src/typescript-nodejs/index.ts
+++ src/typescript-nodejs/index.ts
@@ -1,3 +1,4 @@
+import { join } from "path";
 import { buildActions } from "../lib/buildActions";
 import { ExtendedPlopGeneratorConfig } from "../lib/types";
 
@@ -25,6 +26,13 @@ export const typescriptNodejs: ExtendedPlopGeneratorConfig = {
       templateFiles: "**/*",
       base: "typescript-nodejs/templates",
       globOptions: { dot: true },
+    },
+    // We can't just include the .gitignore file in the templates directory because when publishing
+    // to npm all .gitignore files are removed
+    {
+      type: "add",
+      path: join(data.destination, ".gitignore"),
+      templateFile: "typescript-nodejs/.gitignore.hbs",
     },
   ]),
 };
