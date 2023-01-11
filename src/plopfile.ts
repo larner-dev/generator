4855b53430f19881658d46cac94030857067af94
Aaron Larner <alarner@users.noreply.github.com>
2023-01-11 10:01:04 -0800

    Remove references to typescript-react

--- src/plopfile.ts
+++ src/plopfile.ts
@@ -1,8 +1,6 @@
 import { NodePlopAPI } from "plop";
 import { typescriptNodejs } from "./typescript-nodejs";
-import { typescriptReact } from "./typescript-react";
 
 export default function (plop: NodePlopAPI) {
   plop.setGenerator(typescriptNodejs.generatorName, typescriptNodejs);
-  plop.setGenerator(typescriptReact.generatorName, typescriptReact);
 }
